18eebc27bb2568aa5a9692fe1352c4a6e61b9837
Maksim Rybakov <m.p.rybakov@gmail.com>
2025-04-09 20:40:42 +0300

    add InternshipScreen

--- app/src/main/java/ru/kingofraccoons/crazystudent/presentation/screens/main/profile/student/StudentProfileScreen.kt
+++ app/src/main/java/ru/kingofraccoons/crazystudent/presentation/screens/main/profile/student/StudentProfileScreen.kt
@@ -1,14 +1,39 @@
 package ru.kingofraccoons.crazystudent.presentation.screens.main.profile.student
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ru.kingofraccoons.crazystudent.NavigationFun
+import ru.kingofraccoons.crazystudent.presentation.elements.BodyText
+import ru.kingofraccoons.crazystudent.presentation.elements.Caption1Text
+import ru.kingofraccoons.crazystudent.ui.theme.CrazyStudentTheme
 
 @Composable
 fun StudentProfileScreen(
     navigateToStudyPlane: NavigationFun,
     navigateToAdditionEducation: NavigationFun,
     navigateToRecordBook: NavigationFun,
-    navigateToPreferences: NavigationFun
+    navigateToPreferences: NavigationFun,
 ) {
-
 }
